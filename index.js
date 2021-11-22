09900da3c976600133328846729f60f299973b5e
Ryan Osmundson <rosmundson@outlook.com>
2021-11-22 10:41:45 -0700

    commit tree f9e816eac4278552555287826efb22d1f74679d1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    }
+
+function fuzzyMatch(drivers, name){
+  return drivers.filter(person => person[0].toLowerCase() === name[0].toLowerCase())}
+
+  function matchName(drivers, name){
+    return drivers.filter(person => person.name === name)
+  }
